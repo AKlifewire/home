dc92d3f9a4c89aad2ddbc114ceb41b2f7fa07e6d
ak_lifewire_app <kareemibrahim254@gmail.com>
2025-06-19 13:02:44 +0100

    Remove Flutter application files

--- smart_home_flutter/lib/services/storage_interface.dart
+++
@@ -1,6 +0,0 @@
-abstract class StorageInterface {
-  Future<void> saveValue(String key, String value);
-  Future<String?> getValue(String key);
-  Future<void> deleteValue(String key);
-  Future<void> deleteAll();
-}

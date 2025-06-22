986c13036aaed8bb26b01b83e443f5c1cd8f3253
ak_lifewire_app <kareemibrahim254@gmail.com>
2025-06-22 01:57:50 +0100

    chore: Clean up codebase and fix TypeScript errors

--- cdk/utils/param-paths.ts
+++ cdk/utils/param-paths.ts
@@ -1,1 +1,11 @@
-// Central registry for SSM paths
+export const SSM_PATHS = {
+  USER_POOL_ID: '/SmartHome/{envName}/USER_POOL_ID',
+  USER_POOL_CLIENT_ID: '/SmartHome/{envName}/USER_POOL_CLIENT_ID',
+  IDENTITY_POOL_ID: '/SmartHome/{envName}/IDENTITY_POOL_ID',
+  FRONTEND_DOMAIN: '/SmartHome/{envName}/FRONTEND_DOMAIN',
+  AMPLIFY_APP_ID: '/SmartHome/{envName}/AMPLIFY_APP_ID',
+  APPSYNC_URL: '/SmartHome/{envName}/APPSYNC_URL',
+  APPSYNC_API_KEY: '/SmartHome/{envName}/APPSYNC_API_KEY',
+  IOT_ENDPOINT: '/SmartHome/{envName}/IOT_ENDPOINT',
+  UI_BUCKET: '/SmartHome/{envName}/UI_BUCKET',
+};
